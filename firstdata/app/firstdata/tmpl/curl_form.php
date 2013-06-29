8ee9f2358c5fc29d76c72601ecf485675f476bdf
Chris Zietlow <chris.zietlow@morris.com>
2013-06-29 05:06:03 -0400

    automatically clicks continue button if status is approved

--- firstdata/app/firstdata/tmpl/curl_form.php
+++ firstdata/app/firstdata/tmpl/curl_form.php
@@ -5,20 +5,23 @@
 * @contact 		chris.zietlow@morris.com
 */
 if(defined('_JEXEC')===false) die();?>
-<script>
-window.onload = function() 
-{	
-	  setTimeout("paypalSubmit()", 2000);
-}
+<script type="text/javascript">
+jQuery(document).ready(function() {
+	tranStatus = $('input[name="r_approved"]').val();
+	
+	//successful transaction
+	if (tranStatus == "APPROVED") {
+		$('input[name="Checkout2"]').trigger('click');
+	}
 
-function paypalSubmit()
-{
-// 	document.forms["resForm"].submit();
-// 	document.forms[1].submit();
-// 	document.getElementById('resForm').hide();
-}
+});
+
 </script>
+
+
 	<?php
+	//using for debugging only
+	
 	if ($identifier == TRUE && empty($_POST['status'])) {
 		curl_exec($ch);
 		curl_close($ch); 
